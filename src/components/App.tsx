5238d0bba6ca712e1374301da2d59bffab2bf202
Paulo Serrano <pvsserrano@gmail.com>
2024-02-25 11:59:16 -0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import Home from '../pages/Home'
+import GlobalStyle from '../main.styles'
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Home />
+    </>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import BeLogo from "../assets/be-logo.svg"
+import styled from "styled-components"
+
+export default function Header() {
+  return (
+    <Wrapper>
+      <img src={BeLogo} alt="logo" />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  padding-inline: 2rem;
+  margin-bottom: 2rem;
+  background-color: #fff;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.20);
+`
